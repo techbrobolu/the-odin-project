b1e4524be9a8ad5eedbaf845ccb7fa12d8e4f56b
techbrobolu <197986161+techbrobolu@users.noreply.github.com>
2026-05-12 15:55:12 +0100

    Create join strings program
    Declared variables for first name, last name, this year, and birth year. Then created a variable `fullName` by joining the first and last names, and a variable `age` by subtracting the birth year from the current year. Finally, created a variable `greeting` that combines all the other variables into a single string.
    
    Resolves #4

---
+++ javascript/foundations/join-strings.js
@@ -0,0 +1,28 @@
+/*
+ First name: Carlos
+ Last name: Stevenson
+ This year: 1965
+ Birth year: 1947
+
+ The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
+*/
+
+// Add your code right below, good luck!
+let firstName = "Carlos";
+let lastName = "Stevenson";
+let thisYear = 1965;
+let birthYear = 1947;
+let fullName = firstName + " " + lastName;
+let age = thisYear - birthYear;
+let greeting = "Hello! My name is " + fullName + " and I am " + age + " years old.";
+
+
+module.exports = {
+  firstName: typeof firstName === 'undefined' ? undefined : firstName,
+  lastName: typeof lastName === 'undefined' ? undefined : lastName,
+  thisYear: typeof thisYear === 'undefined' ? undefined : thisYear,
+  birthYear: typeof birthYear === 'undefined' ? undefined : birthYear,
+  greeting: typeof greeting === 'undefined' ? undefined : greeting,
+  fullName: typeof fullName === 'undefined' ? undefined : fullName,
+  age: typeof age === 'undefined' ? undefined : age
+}
