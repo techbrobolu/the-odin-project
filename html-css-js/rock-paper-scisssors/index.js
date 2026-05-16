7a9fb6ca921c60487a8e3e4259427d7eab17f362
techbrobolu <197986161+techbrobolu@users.noreply.github.com>
2026-05-16 10:49:44 +0100

    Write computer choice funtion
    Created function `getComputerChoice` that decides between `rock`, `paper`, or `scissors` depending on the result of a mathe matical calculation. The calculation involves the rounding up of the multiplication of a random number, derived using the method `Math.random`, by another random number and 10. Depending on whether the result is within a certain range, the function returns a choice.

--- html-css-js/rock-paper-scisssors/index.js
+++ html-css-js/rock-paper-scisssors/index.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+function getComputerChoice() {
+	let min = 1;
+	let max = 10;
+	let randomInt = Math.floor(Math.random() * (max - min + 1)) + min;
+
+	if (randomInt >= 0 && randomInt < 3) {
+		return "rock";
+	} else if (randomInt >= 3 && randomInt < 6) {
+		return "paper";
+	} else if (randomInt >= 6 && randomInt <= 10) {
+		return "scissors";
+	}
+}
+
+console.log(getComputerChoice());
