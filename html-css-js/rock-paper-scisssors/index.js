fc55d1f6bc83c8f2c8556c93aa9bca8a41cccf22
techbrobolu <197986161+techbrobolu@users.noreply.github.com>
2026-05-16 12:12:12 +0100

    Write logic to get human choice
    Created a function called getHumanChoice that prompts the user to enter their choice of "rock", "paper", or "scissors". The function validates the input and return the user's choice in lowercase. If the input is invalid, it should prompt the user again until a valid choice is entered.

--- html-css-js/rock-paper-scisssors/index.js
+++ html-css-js/rock-paper-scisssors/index.js
@@ -3,13 +3,28 @@ function getComputerChoice() {
 	let max = 10;
 	let randomInt = Math.floor(Math.random() * (max - min + 1)) + min;
 
-	if (randomInt >= 0 && randomInt < 3) {
+	if (randomInt >= 0 && randomInt < 4) {
 		return "rock";
-	} else if (randomInt >= 3 && randomInt < 6) {
+	} else if (randomInt >= 4 && randomInt < 7) {
 		return "paper";
-	} else if (randomInt >= 6 && randomInt <= 10) {
+	} else if (randomInt >= 7 && randomInt <= 10) {
 		return "scissors";
 	}
 }
 
+function getHumanChoice(){
+	let promptInput = prompt("Choose Rock, Paper or Scissors")
+
+	if (promptInput.toLowerCase() === "rock") {
+		return "rock";
+	} else if (promptInput.toLowerCase() === "scissors") {
+		return "scissors";
+	} else if (promptInput.toLowerCase() === "paper") {
+		return "paper";
+	} else{
+		return "rock"
+	}
+}
+
 console.log(getComputerChoice());
+console.log(getHumanChoice())
