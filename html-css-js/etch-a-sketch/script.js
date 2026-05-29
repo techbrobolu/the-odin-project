76492d80821b4d078c8f7ea4dff4aa25306e63d1
techbrobolu <197986161+techbrobolu@users.noreply.github.com>
2026-05-29 13:28:47 +0100

    Add grid resize functionality
    The user should be able to resize the grid after it has been created. This allows for more flexibility and customization of the drawing experience on the etch-a-sketch application.
    
    - Added a button to the HTML that will trigger the grid resize functionality.
    - Created a function in the JavaScript that will prompt the user for a new grid size and then update the grid accordingly.
    - Ensured that the new grid size is validated to prevent errors and that the grid is properly cleared and redrawn with the new size.
    
    Resolves #72

--- html-css-js/etch-a-sketch/script.js
+++ html-css-js/etch-a-sketch/script.js
@@ -1,12 +1,26 @@
 let gridCanvas = document.querySelector(".grid-canvas");
+let resizeBtn = document.querySelector(".resize-btn");
 
+resizeBtn.addEventListener("click", () => {
+	let newSize = parseInt(prompt("What grid size do you want?"));
+    // Persists in asking for the right input in case the user enters one that doesn't match the conditions
+	while (isNaN(newSize) || (newSize < 1 && newSize > 100)) {
+		newSize = parseInt(prompt("Try Again!! It should be a number from 1 to 100"));
+	}
+    // This removes every grid cell by removing the firstChild of th grid canvas until there's no child
+	while (gridCanvas.firstChild) {
+		gridCanvas.removeChild(gridCanvas.firstChild);
+	}
+
+	createGrid(newSize);
+});
 function createGrid(size) {
 	gridCanvas.style.display = "grid";
-    let cellNum = 0
+	let cellNum = 0;
 
-	for (let i = 0; i < size; i++) {
-		for (let j = 1; j <= size; j++) {
-            cellNum++
+	for (let i = 0; i < size; i++) {   // This serves as the row coordinate
+		for (let j = 1; j <= size; j++) {    // This serves as the column coordinate
+			cellNum++;
 			let div = document.createElement("div");
 			div.classList.add("grid-cell", `grid-cell-${cellNum}`);
 			div.textContent = `grid-cell-${cellNum}`;
