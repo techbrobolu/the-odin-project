d6958cb1243aaa18221ecea0e235df9cdfecdd55
techbrobolu <197986161+techbrobolu@users.noreply.github.com>
2026-05-29 04:34:29 +0100

    Implement hover to draw interaction
    The hover to draw interaction allows users to draw on the canvas by simply hovering their mouse over the grid cells. This feature enhances the user experience by providing a more intuitive and seamless way to create drawings without the need for clicking.
    
    To implement the hover to draw interaction, we will add an event listener to each grid cell that listens for the `mouseover` event. When the user hovers over a cell, we will change its background color to simulate drawing.
    
    Resolves #71

--- html-css-js/etch-a-sketch/script.js
+++ html-css-js/etch-a-sketch/script.js
@@ -1,13 +1,23 @@
-let gridCanvas = document.querySelector('.grid-canvas');
+let gridCanvas = document.querySelector(".grid-canvas");
 
-function createGrid(size){
-    for (let i = 1; i <= size; i++) {
-        let div = document.createElement("div");
-        div.classList.add('grid-cell', `grid-cell-${i}`)
-        gridCanvas.appendChild(div)
-    }
+function createGrid(size) {
+	gridCanvas.style.display = "grid";
+    let cellNum = 0
 
-    gridCanvas.style.gridTemplateColumns = size
+	for (let i = 0; i < size; i++) {
+		for (let j = 1; j <= size; j++) {
+            cellNum++
+			let div = document.createElement("div");
+			div.classList.add("grid-cell", `grid-cell-${cellNum}`);
+			div.textContent = `grid-cell-${cellNum}`;
+			div.addEventListener("mouseover", (e) => {
+				e.target.style.background = "red";
+			});
+			gridCanvas.appendChild(div);
+		}
+	}
+
+	gridCanvas.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr)`;
 }
 
-createGrid(16)
+createGrid(16);
