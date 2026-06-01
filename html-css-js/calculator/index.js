d3244a8912daab1999cef78cbcf431c617dbf50d
techbrobolu <197986161+techbrobolu@users.noreply.github.com>
2026-06-01 22:36:45 +0100

    Implement operate() dispatcher function

--- html-css-js/calculator/index.js
+++ html-css-js/calculator/index.js
@@ -2,8 +2,24 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+function operate(operator, a, b){
+    switch (operator) {
+        case "+":
+            return add(a,b)
+            break;
+        case "-":
+            return subtract(a,b)
+            break;
+        case "*":
+            return multiply(a,b)
+            break;
+        case "/":
+            return divide(a,b)
+            break;
+    }
+}
 
-console.log(add(5, 2));
-console.log(subtract(5, 2));
-console.log(multiply(5, 2));
-console.log(divide(5, 2));
+console.log(operate("+", 5, 2));
+console.log(operate("-", 5, 2));
+console.log(operate("*", 5, 2));
+console.log(operate("/", 5, 2));
