0c3ed154ea1d496671f566202dc0a583cb6263f2
techbrobolu <197986161+techbrobolu@users.noreply.github.com>
2026-06-01 22:11:51 +0100

    Implement core math functions
    Resolves #101

---
+++ html-css-js/calculator/index.js
@@ -0,0 +1,9 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+console.log(add(5, 2));
+console.log(subtract(5, 2));
+console.log(multiply(5, 2));
+console.log(divide(5, 2));
