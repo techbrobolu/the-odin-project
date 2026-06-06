9d4e20766f03dfd916e00db4a7c4edfc9d652692
techbrobolu <197986161+techbrobolu@users.noreply.github.com>
2026-06-06 06:02:46 +0100

    Implement operator selection and calculation logic
    - There are now functions for each operator and an operate function that takes in an operator and two operands and performs the correct operation based on the operator.
    
    - The calculator can now perform calculations when the equals button is pressed.
    
    - The result is displayed in the result display and the working display shows the current problem being solved.
    
    - The calculator also handles decimal numbers and displays them with a maximum of 4 decimal places if necessary.
    
    Resolves #105

--- html-css-js/calculator/index.js
+++ html-css-js/calculator/index.js
@@ -1,39 +1,106 @@
 const calcBtns = document.querySelector(".calc-buttons");
 const acBtn = document.querySelector(".ac");
-const delBtn = document.querySelector(".del")
+const delBtn = document.querySelector(".del");
 const workingDisplay = document.querySelector(".working");
 workingDisplay.textContent = "";
 const resultDisplay = document.querySelector(".result");
-resultDisplay.textContent = "0"
+resultDisplay.textContent = "0";
+let currentProblem = { firstOperand: "", operator: "", secondOperand: "" };
+let problem;
+let currentNum;
+let currentOperator;
+let currentResult;
 
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
-function operate(operator, a, b){
-    switch (operator) {
-        case "+":
-            return add(a,b)
-            break;
-        case "-":
-            return subtract(a,b)
-            break;
-        case "*":
-            return multiply(a,b)
-            break;
-        case "/":
-            return divide(a,b)
-            break;
-    }
+function operate(operator, a, b) {
+	switch (operator) {
+		case "+":
+			return add(parseInt(a), parseInt(b));
+			break;
+		case "-":
+			return subtract(parseInt(a), parseInt(b));
+			break;
+		case "×":
+			return multiply(parseInt(a), parseInt(b));
+			break;
+		case "/":
+			return divide(parseInt(a), parseInt(b));
+			break;
+	}
 }
 
 calcBtns.addEventListener("click", (e) => {
-    let button = e.target
+	let button = e.target;
 
-    if (button.classList.contains("num-btn")){
-        workingDisplay.textContent += button.dataset.value
-        resultDisplay.textContent = "0";
-    } else if (button.classList.contains("operator")){
-        workingDisplay.textContent += button.dataset.value
-    }
-})
+	// Check if the button is a number button or an operator button
+	switch (button.classList[1]) {
+		case "num-btn":
+			currentNum = button.dataset.value;
+			// If there's no operator selected, add the number to the first operand otherwise add it to the second operand
+			if (!currentProblem.operator) {
+				currentProblem.firstOperand += currentNum;
+			} else {
+				currentProblem.secondOperand += currentNum;
+			}
+
+			workingDisplay.textContent += currentNum;
+			currentNum = "";
+			break;
+		case "operator":
+			currentOperator = button.dataset.value;
+            // If there's no second operand, set the operator to the current operator otherwise perform the operation and set the result as the first operand and set the operator to the current operator
+			if (!currentProblem.secondOperand) {
+				currentProblem.operator = currentOperator;
+			} else {
+				problem = [
+					currentProblem.operator,
+					currentProblem.firstOperand,
+					currentProblem.secondOperand,
+				];
+				console.log(problem);
+				currentResult = operate(...problem);
+				currentProblem.firstOperand = currentResult;
+				currentProblem.operator = currentOperator;
+				currentOperator = "";
+				currentProblem.secondOperand = "";
+				console.log(currentProblem);
+			}
+
+			workingDisplay.textContent += currentProblem.operator;
+			break;
+	}
+
+	switch (button.dataset.value) {
+		// If the button is the equal sign, perform the operation
+		case "=":
+			problem = [
+				currentProblem.operator,
+				currentProblem.firstOperand,
+				currentProblem.secondOperand,
+			];
+            // If there's no second operand, set the result to the first operand otherwise perform the operation and set the result to the current result
+			currentResult =
+				currentProblem.secondOperand ? operate(...problem)
+				: currentProblem.firstOperand ? currentProblem.firstOperand
+				: 0;
+			let decimal = currentResult - currentResult.toFixed();
+			console.log(String(decimal).length);
+			if (decimal !== 0) {
+				resultDisplay.textContent =
+					String(decimal).length > 5 ?
+						currentResult.toFixed(4)
+					:	currentResult.toFixed(2);
+			} else {
+				resultDisplay.textContent = currentResult;
+			}
+
+			break;
+	}
+});
+
+// For firstnum to exist, secondnum and op have to be empty
+// For operator to exist, firstnum have to be filled and secondnum has to be empty
+// For secondnum to exist, firstnum and op have to be filled
